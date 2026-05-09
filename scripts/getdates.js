07bf768334adc552d8d414280f38a3e0c8f6f1e6
jobrodne <jrcademus87@gmail.com>
2026-05-09 11:04:31 -0500

    home page

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+// Current Year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Last Modified
+document.getElementById("lastModified").textContent =
+    "Last Modified: " + document.lastModified;
